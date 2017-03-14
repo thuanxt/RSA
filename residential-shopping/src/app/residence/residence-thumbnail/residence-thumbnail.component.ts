f9e80999c5c8860ddf09956ea4af66fbe489080c
Xinh Thanh Thuan <thuanxt@outlook.com>
2017-03-15 00:25:56 +0700

    model: add residence description component: add Residence model and define as input html: add residence information

--- residential-shopping/src/app/residence/residence-thumbnail/residence-thumbnail.component.ts
+++ residential-shopping/src/app/residence/residence-thumbnail/residence-thumbnail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Residence } from '../shared/residence.model';
 
 @Component({
 	selector: 'residence-thumbnail',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ResidenceThumbnailComponent implements OnInit {
+	@Input() residence : Residence;
 
 	constructor() { }
 
--- residential-shopping/src/app/residence/shared/residence.model.ts
+++ residential-shopping/src/app/residence/shared/residence.model.ts
@@ -2,5 +2,6 @@ export class Residence {
 	id: number;
 	name: string;
 	address: string;
+	description: string;
 	imageUrl: string;
 }
