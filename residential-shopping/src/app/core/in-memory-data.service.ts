7494db61c79e9bab9f4dd1e2319d5e96f0c72b7a
Xinh Thanh Thuan <thuanxt@outlook.com>
2017-03-18 16:51:07 +0700

    serve residence image

--- residential-shopping/src/app/core/in-memory-data.service.ts
+++ residential-shopping/src/app/core/in-memory-data.service.ts
@@ -8,17 +8,68 @@ export class InMemoryDataService implements InMemoryDbService {
         name: 'The Park Residence', 
         address: 'Nha Be',
         description: 'Nha Be',
-        imageUrl: '#'
+        imageUrl: '/assets/img/residence/the-park-residence.jpg'
       },
-      {id: 12, name: 'Park Vista'},
-      {id: 13, name: 'Sunrise City'},
-      {id: 14, name: 'Sun River Side'},
-      {id: 15, name: 'Lexington'},
-      {id: 16, name: 'Imperia'},
-      {id: 17, name: 'Dragon Hill'},
-      {id: 18, name: 'The Sun Avenue'},
-      {id: 19, name: 'IDICO'},
-      {id: 20, name: 'Lotus'}
+      {
+        id: 12,
+        name: 'Park Vista',
+        address: 'Nha Be',
+        description: 'Nha Be',
+        imageUrl: '/assets/img/residence/park-vista.jpg'
+      },
+      {
+        id: 13, 
+        name: 'Sunrise City',
+        address: 'Nha Be',
+        description: 'Nha Be',
+        imageUrl: '/assets/img/residence/sunrise-city.jpg'
+      },
+      {
+        id: 14, 
+        name: 'Sun River Side',
+        address: 'Nha Be',
+        description: 'Nha Be',
+        imageUrl: '/assets/img/residence/the-park-residence.jpg'
+      },
+      {
+        id: 15, 
+        name: 'Lexington',
+        address: 'Nha Be',
+        description: 'Nha Be',
+        imageUrl: '/assets/img/residence/lexington.jpg'
+      },
+      {
+        id: 16, 
+        name: 'Imperia',
+        address: 'Nha Be',
+        description: 'Nha Be',
+        imageUrl: '/assets/img/residence/imperia.jpg'
+      },
+      { 
+        id: 17,
+        name: 'Dragon Hill',
+        address: 'Nha Be',
+        description: 'Nha Be',
+        imageUrl: '/assets/img/residence/dragon-hill.jpg'
+      },
+      {
+        id: 18, name: 'The Sun Avenue',
+        address: 'Nha Be',
+        description: 'Nha Be',
+        imageUrl: '/assets/img/residence/the-sun-avenue.jpg'
+      },
+      {
+        id: 19, name: 'IDICO',
+        address: 'Nha Be',
+        description: 'Nha Be',
+        imageUrl: '/assets/img/residence/idico.jpg'
+      },
+      {
+        id: 20, name: 'Lotus',
+        address: 'Nha Be',
+        description: 'Nha Be',
+        imageUrl: '/assets/img/residence/lotus.jpg'
+      }
     ];
     return {residence};
   }
